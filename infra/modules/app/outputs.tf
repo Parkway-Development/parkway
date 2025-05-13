a22d24f5021b39ca52aa442a1175bae70c17f788
jaydavis <jay@jaydavis.us>
2025-05-13 14:39:36 -0400

    removed terraform stuff

--- infra/modules/app/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "api_app_url" {
-  value = azurerm_linux_web_app.api.default_hostname
-}
-
-output "frontend_app_url" {
-  value = azurerm_linux_web_app.frontend.default_hostname
-}
--- infra/modules/core/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.main.name
-}
-
-output "key_vault_id" {
-  value = azurerm_key_vault.kv.id
-}
